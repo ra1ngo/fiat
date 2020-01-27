9e7a59690924a3c35a2a53119075f2319509e2a4
ra1ngo <raingo@list.ru>
2020-01-27 19:47:45 +0400

    stack to viewBuilder

--- app/src/main/java/ru/raingo/fiat/yar/ViewBuilder.kt
+++ app/src/main/java/ru/raingo/fiat/yar/ViewBuilder.kt
@@ -4,55 +4,68 @@ import android.content.Context
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 
 class ViewBuilder(val context: Context) {
     val TAG = "YarViewBuilder"
-    private var view: ViewGroup? = null
-    private var currentNode: Node? = null
+    private lateinit var root: ViewGroup
+    private val stack: MutableList<View> = mutableListOf()
 
     fun build(ast: Node): ViewGroup? {
         processingAst(ast)
 
-        return view
+        return root
     }
 
-    private fun processingAst(parent: Node) {
-        processingNode(parent)
-        for (node in parent.nodes) {
-            processingAst(node)
+    private fun processingAst(node: Node) {
+        val view = processingNode(node)
+        stack.add(view)
+        for (child in node.nodes) {
+            processingAst(child)
         }
+        stack.removeAt(stack.size - 1)
     }
 
-    private fun processingNode(node: Node) {
-        val action = actions[node.tag] ?: return
-        currentNode = node
-        action()
+    private fun processingNode(node: Node): View {
+        val action = actions[node.tag] ?: return View(context)
+        return action(node)
     }
 
     //ACTIONS
-    val actions = mapOf<Tag, ()->Unit>(
+    val actions = mapOf<Tag, (Node)->View>(
         Tag.ROOT to ::onRoot,
         Tag.LAY to ::onLay,
         Tag.TEXT to ::onText
     )
 
-    fun onRoot() {
+    fun onRoot(node: Node): ViewGroup {
         Log.d(TAG, "onStart")
+        root = FrameLayout(context)
+
+        return root
     }
 
-    fun onLay() {
+    fun onLay(node: Node): ViewGroup {
         Log.d(TAG, "onLay")
-        view = LinearLayout(context)
+        val view = LinearLayout(context)
+
+        val parent = stack[stack.size - 1] as ViewGroup
+        parent.addView(view)
+
+        return view
     }
 
-    fun onText() {
+    fun onText(node: Node): TextView {
         Log.d(TAG, "onText")
-        val childView = TextView(context)
-        childView.text = currentNode?.textList?.joinToString("","","")
-        childView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        val view = TextView(context)
+        view.text = node.textList.joinToString("","","")
+        view.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
 
-        view?.addView(childView)
+        val parent = stack[stack.size - 1] as ViewGroup
+        parent.addView(view)
+
+        return view
     }
 }
